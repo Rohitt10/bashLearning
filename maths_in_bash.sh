af9060fb98982951d8a4dc164a3918e26117a3a2
Rohit Mishra <rrohitt33@gmail.com>
2025-01-06 02:29:39 +0530

    feat: added math in bash examples

---
+++ maths_in_bash.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+# learning maths in bash
+
+expr 10 + 10 
+expr 10 - 10 
+expr 10 / 10
+expr 10 \* 10 # why beacuse * is wildcard can not be used directly
+num1=10
+num2=30
+expr $num1 + $num2
