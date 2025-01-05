0e60ef3f418d4bb2d641fc184804ffd69f3b4038
Rohit Mishra <rrohitt33@gmail.com>
2025-01-06 02:19:10 +0530

    feat: added variable usage examples

---
+++ variables.sh
@@ -0,0 +1,7 @@
+name="Rohit"
+ocuupation="Software Engineer"
+directory=$(pwd) # capturing output of a command in a variable
+echo "Hello my name is $name"
+echo "I work as $ocuupation "
+echo "you are executing this command in $directory directory"
+echo "your username is $USER" # here USER is environment variable
